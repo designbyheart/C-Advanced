0f5b39d3dea2305a2eb510d4a51bc17c2ef31aaa
Pedja Jevtic <designbyheart@gmail.com>
2023-11-08 15:20:47 +0100

    Added asyncAwait

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncAwaitDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // new Demo1();
+            new Demo2();
+        }
+    }
+}
